b3440b72dda7f028089664ea2b6efac5fc306b24
JohnDurand33 <142426904+JohnDurand33@users.noreply.github.com>
2024-08-19 16:36:04 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
+import { Route, Routes, Router } from 'react-router-dom'
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
-        <>
-
-        </>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    {/* Nested routes */}
+                    <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+            </Routes>
+        </Router>
     )
 }
 
